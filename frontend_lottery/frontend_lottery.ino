c9067b5673372fad9ed6a80edbc1be4841505120
Joshua Miller <jshuasmiller@gmail.com>
2015-05-10 07:29:42 -0500

    update states

--- frontend_lottery/frontend_lottery.ino
+++ frontend_lottery/frontend_lottery.ino
@@ -4,6 +4,7 @@
 typedef struct wheel {
     int pin;
     int offset;
+    int total_offset;
     unsigned char voltage;
     int brake_pin;
     int brake_threshold;
@@ -23,12 +24,10 @@ const char SPIN   = 's';
 const char SPUN   = 't';
 const char LOSE   = 'l';
 
-char state     = OFF;
-char old_state = OFF; //used to track changes of the state
-
+char state = OFF;
 
 /* PINS */
-const int coin_input_pin = 0;
+const int state_button_pin = 2;
 const int ledPin = 13;
 
 /* Wheel init {pin, offset} */
@@ -38,7 +37,7 @@ wheel_t wheels[N_WHEELS] = {
     {7, 0, HIGH, 10, 30}
 };
 
-void test_brake(int i)
+void tap_brake(int i)
 {
     digitalWrite(wheels[i].brake_pin, HIGH);
     delay(50);
@@ -50,14 +49,14 @@ void setup(){
     Serial.begin(9600);
 
     /* initialize pins */
-    pinMode(coin_input_pin, INPUT);
+    pinMode(state_button_pin, INPUT);
     pinMode(ledPin, OUTPUT);
     for (int i = 0; i < N_WHEELS; i++){
         pinMode(wheels[i].pin, INPUT);
         pinMode(wheels[i].brake_pin, OUTPUT);
-        test_brake(i);
+        tap_brake(i);
     }
-    state = SPIN;
+    state = OFF;
 }
 
 void update_wheel_offsets()
@@ -69,6 +68,7 @@ void update_wheel_offsets()
             // the spoke has made contact again
             wheels[i].voltage = HIGH;
             wheels[i].offset++; 
+            wheels[i].total_offset++; 
             digitalWrite(ledPin, HIGH); 
         } if (voltage == LOW && wheels[i].voltage == HIGH){
             // we've gone from high to low voltage, this means that
@@ -100,19 +100,23 @@ void reset_wheels()
 {
     for (int i = 0; i < N_WHEELS; i++){
         digitalWrite(wheels[i].brake_pin, LOW);
+        wheels[i].offset = 0;
     }
 }
 
 void transition_to_state(int _state)
 {
-    //Serial.println(_state);
+    Serial.println(_state);
     state = _state;
 }
 
 void loop() {
-
     if (state == OFF){
-      transition_to_state(SPIN);
+        if (digitalRead(state_button_pin) == HIGH){
+          //digitalWrite(ledPin, HIGH);
+          delay(100);
+          transition_to_state(SPIN);
+        } 
     } else if (state == SPIN){
       update_wheel_offsets();
     } else if (state == SPUN){
