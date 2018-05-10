986b56a6515f09db5455db821e2525fcfd49e6af
eurbs <emileeurbanek@gmail.com>
2018-05-10 11:24:08 -0500

    Add logic for percent chance of winning and bootstrap

--- 2018/needle/src/App.js
+++ 2018/needle/src/App.js
@@ -5,16 +5,29 @@ import './App.css';
 
 
 class App extends Component {
+  _getValue() {
+    const startTimestamp = 1525968282000;
+    const endTimestamp = 1526223600000;
+    const denominator = endTimestamp - startTimestamp;
+
+    const currDate = new Date();
+    const currTime = currDate.getTime();
+    const numerator = currTime - startTimestamp;
+
+    const percentChanceOfWinningFromNegHundoToHundo = 200 - ((numerator/denominator)*200) - 100;
+    return percentChanceOfWinningFromNegHundoToHundo
+  }
+
   constructor() {
     super();
     this.state = {
-      value: 100
+      value: this._getValue()
     }
   }
 
   componentDidMount() {
     console.log("throw me a bone", this.state)
-    this.value = setInterval(() => this.setState({value: this.state.value + 100}), 1000)
+    this.value = setInterval(() => this.setState({value: this._getValue()}), 1000)
   }
 
   componentWillUnmount() {
@@ -31,15 +44,27 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <ReactSpeedometer 
-          value={this.state.value}
-          width={600}
-          height={400}
-          currentValueText={"2018 Scav"}
-          startColor={"#7474ea"}
-          minValue={-100}
-          maxValue={100}
-        />
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <span>Snitchcock</span>
+            </div>
+            <div class="col-6">
+              <ReactSpeedometer 
+                value={this.state.value}
+                width={600}
+                height={400}
+                currentValueText={"2018 Scav"}
+                startColor={"#7474ea"}
+                minValue={-100}
+                maxValue={100}
+              />
+            </div>
+            <div class="col">
+              <span>Breck</span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
